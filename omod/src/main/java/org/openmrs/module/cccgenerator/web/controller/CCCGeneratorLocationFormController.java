b14aad97ac5a668045ba41a6ecf48e1fcf82a568
Jeremy Keiper <jeremy@openmrs.org>
2012-11-29 14:28:36 -0500

    fixed location form to allow editing of existing CCC location prefixes

--- omod/src/main/java/org/openmrs/module/cccgenerator/web/controller/CCCGeneratorLocationFormController.java
+++ omod/src/main/java/org/openmrs/module/cccgenerator/web/controller/CCCGeneratorLocationFormController.java
@@ -1,10 +1,5 @@
 package org.openmrs.module.cccgenerator.web.controller;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Location;
@@ -13,111 +8,112 @@ import org.openmrs.api.context.Context;
 import org.openmrs.module.cccgenerator.model.CCCCount;
 import org.openmrs.module.cccgenerator.model.CCCLocation;
 import org.openmrs.module.cccgenerator.service.CCCGeneratorService;
+import org.openmrs.web.WebConstants;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 @Controller
+public class CCCGeneratorLocationFormController {
 
-	
-	
-	public class CCCGeneratorLocationFormController {
-	private final String SUCCESS_FORM_VIEW="module/cccgenerator/cccgeneratorForm.form";
-		
-		/** Logger for this class and subclasses */
-		protected final Log log = LogFactory.getLog(getClass());
-		
-		@RequestMapping(method=RequestMethod.GET,value = "module/cccgenerator/cccgeneratorLocationForm.form")
-        public void whenPageLoads(ModelMap map) {
-            CCCGeneratorService service=Context.getService(CCCGeneratorService.class);
-			
-			List<CCCLocation> cccLocation=service.getAllCCCLocations();
-            
-			List<Location> location=new ArrayList<Location>(); 
-            //CCCLocation=service.getAllCCCLocations();    
-            
-            
-            for(CCCLocation mlocation:cccLocation){
-            	location.add(Context.getLocationService().getLocation(mlocation.getLocation().getLocationId()));
-            	
-            }
-            	
-            	
-			List<Location> locationsList = Context.getLocationService().getAllLocations();
-			locationsList.removeAll(location);
+	private final String SUCCESS_FORM_VIEW = "module/cccgenerator/cccgeneratorLocationForm.form";
 
-            Collections.sort(locationsList, new SortLocationsByLocationIdComparator());    
-                //map.addAttribute("location", location);
-                
-                map.addAttribute("locationList", locationsList);
-                
-        }
-		
-		
-		@RequestMapping(method=RequestMethod.POST,value = "module/cccgenerator/cccgeneratorLocationForm.form")
-         public void processForm(ModelMap map,
-        		 @RequestParam(required=true, value="location_id") String location_id,
-        		 @RequestParam(required=true, value="CCC") String ccc
-        		 ) {
+	/**
+	 * Logger for this class and subclasses
+	 */
+	protected final Log log = LogFactory.getLog(getClass());
 
-			 LocationService locService=Context.getLocationService();
-			 
-			 
-			 
-			 CCCGeneratorService service=Context.getService(CCCGeneratorService.class);
-			 
-			 CCCLocation cccLocation1= new CCCLocation();
-			 CCCCount cccCount=new CCCCount();
+	@RequestMapping(method = RequestMethod.GET, value = "module/cccgenerator/cccgeneratorLocationForm.form")
+	public void whenPageLoads(ModelMap map) {
+		CCCGeneratorService service = Context.getService(CCCGeneratorService.class);
 
+		// get all locations from the db
+		List<Location> locations = Context.getLocationService().getAllLocations(false);
 
-			 cccLocation1.setLocation(locService.getLocation(Integer.parseInt(location_id)));
-			 cccLocation1.setCCC(Integer.parseInt(ccc));
-
-			 cccCount.setCCC(Integer.parseInt(ccc));
-			 cccCount.setLastCount(0);
-             cccCount.setLocation(Integer.parseInt(location_id));
-
-
-
-			 service.saveCCCLocation(cccLocation1);
-			 service.saveCCCCount(cccCount);
-             //service.saveAllRelatedSites(0,CCC);
-			
-			 List<CCCLocation> CCCLocation=service.getAllCCCLocations();
-	            
-				List<Location> location=new ArrayList<Location>(); 
-	            //CCCLocation=service.getAllCCCLocations();
-
-
-	            for(CCCLocation mlocation:CCCLocation){
-	            	location.add(Context.getLocationService().getLocation(mlocation.getLocation().getLocationId()));
-	            	
-	            }
-	            	
-	            	
-				List<Location> locationsList = Context.getLocationService().getAllLocations();
-				locationsList.removeAll(location);
-	            Collections.sort(locationsList, new SortLocationsByLocationIdComparator());    
-	                //map.addAttribute("location", location);
-	                
-	                map.addAttribute("locationList", locationsList);
-			 //return "redirect:CCCgeneratorForm.form?CCCId="+CCCLocation.getCCC();
-		 }
-		 
-	private static class SortLocationsByLocationIdComparator implements Comparator<Object>{
-
-		public int compare(Object a, Object b) {
-			Location ae=(Location) a; 
-			Location be=(Location) b;
-		
-			
-			return ae.getLocationId().compareTo(be.getLocationId());
+		// build a list of CCCLocation objects corresponding to locations
+		List<CCCLocation> cccLocations = new ArrayList<CCCLocation>();
+		for (Location location : locations) {
+			CCCLocation cccLocation = service.getCCCLocationByLocation(location);
+			if (cccLocation == null) {
+				// create a blank un-persisted CCCLocation as a holder
+				cccLocation = new CCCLocation();
+				cccLocation.setLocation(location);
+			}
+			cccLocations.add(cccLocation);
 		}
-		
+
+		// sort locations alphabetically and add to model
+		Collections.sort(cccLocations, new SortLocationsByLocationNameComparator());
+		map.addAttribute("locationList", cccLocations);
 	}
 
+	@RequestMapping(method = RequestMethod.POST, value = "module/cccgenerator/cccgeneratorLocationForm.form")
+	public void processForm(HttpServletRequest request, ModelMap map,
+	                        @RequestParam(required = true, value = "location_id") Integer location_id,
+	                        @RequestParam(required = true, value = "CCC") Integer ccc
+	) {
+		CCCGeneratorService service = Context.getService(CCCGeneratorService.class);
+		LocationService locService = Context.getLocationService();
+		HttpSession httpSession = request.getSession();
 
+		// get the related Location
+		Location location = locService.getLocation(location_id);
+		if (location == null) {
+			httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, "Location " + location_id + " could not be found.");
+			return;
+		}
 
+		// get the related CCCLocation
+		CCCLocation cccLocation = service.getCCCLocationByLocation(location);
+
+		// if it doesn't exist, create a new one
+		if (cccLocation == null) {
+			cccLocation = new CCCLocation();
+			cccLocation.setLocation(location);
+		}
+
+		// set the CCC number
+		cccLocation.setCCC(ccc);
+
+		// save it
+		service.saveCCCLocation(cccLocation);
+
+		// create a new CCCCount
+		CCCCount cccCount = new CCCCount();
+
+		// initialize it
+		cccCount.setCCC(ccc);
+		cccCount.setLastCount(0);
+		cccCount.setLocation(cccLocation.getId());
+
+		// save it
+		service.saveCCCCount(cccCount);
+		httpSession.setAttribute(WebConstants.OPENMRS_MSG_ATTR, "Location " + location + " saved with CCC code: " + ccc + ".");
+
+		return;
+	}
+
+	private static class SortLocationsByLocationNameComparator implements Comparator<Object> {
+
+		public int compare(Object a, Object b) {
+			CCCLocation ae = (CCCLocation) a;
+			CCCLocation be = (CCCLocation) b;
+
+			try {
+				return ae.getLocation().getName().compareTo(be.getLocation().getName());
+			} catch (NullPointerException ex) {
+				return 0;
+			}
+		}
+	}
 }
 
--- omod/src/main/webapp/cccgeneratorLocationForm.jsp
+++ omod/src/main/webapp/cccgeneratorLocationForm.jsp
@@ -1,46 +1,43 @@
-<%@ include file="/WEB-INF/template/include.jsp"%>
+<%@ include file="/WEB-INF/template/include.jsp" %>
 
 <openmrs:require privilege="Manage CCC Numbers" otherwise="/login.htm"
-                 redirect="/module/cccgenerator/cccgeneratorLocationForm.form" />
+                 redirect="/module/cccgenerator/cccgeneratorLocationForm.form"/>
 
-<%@ include file="/WEB-INF/template/header.jsp"%>
-<%@ include file="localheader.jsp"%>
+<%@ include file="/WEB-INF/template/header.jsp" %>
+<%@ include file="localheader.jsp" %>
 
-<script type="text/javascript">
-
-</script>
-<b class="boxHeader"><spring:message code="CCC Generator - Code Entry Form" /></b>
-
+<b class="boxHeader"><spring:message code="CCC Generator - Code Entry Form"/></b>
 
 <div class="box">
-<c:choose>
-	<c:when test="${not empty locationList}">
-	
-<table border="0">
-  <tr>
-   <th>Location ID</th>
-   <th>Name</th>
-   <th>Parent Location</th>
-   <th>Enter CCC code</th>
-   <th>Save Entry</th>
-   
-  </tr>
-  <c:forEach var="locations" items="${locationList}">
-      <form name="newCCCLocation"  method="post">
-      <tr align="">
-        <td>${locations.locationId}<input type="hidden" name="location_id" value=${locations.locationId}></td>
-        <td> ${locations.name}</td>
-        <td>${locations.parentLocation}</td>
-        <td><input type="text" name="CCC" size="10" maxlength="5"></td>
-        <td><input type="submit" name="submit" value="Save Entry"></td>
-        </tr> 
-        </form>       
-  </c:forEach>
-</table>
-</c:when>
-<c:otherwise>
-       <p> All CCC numbers have been entered. Please click <a href="cccgeneratorForm.form">here</a>for redirection</p>
-</c:otherwise>
-</c:choose>
+    <c:choose>
+        <c:when test="${not empty locationList}">
+            <table border="0">
+                <tr>
+                    <th>Location</th>
+                    <th>Parent Location</th>
+                    <th>CCC code</th>
+                    <th>Action</th>
+                </tr>
+                <c:forEach var="cccLocation" items="${locationList}">
+                    <c:set var="location" value="${cccLocation.location}"/>
+                    <form name="newCCCLocation" method="post">
+                        <input type="hidden" name="location_id" value=${location.locationId}>
+                        <tr>
+                            <td>${location}</td>
+                            <td>${location.parentLocation}</td>
+                            <td><input type="text" name="CCC" size="10" maxlength="5" value="${cccLocation.CCC}"/></td>
+                            <td><input type="submit" name="submit" value="Save Entry"/></td>
+                        </tr>
+                    </form>
+                </c:forEach>
+            </table>
+        </c:when>
+        <c:otherwise>
+            <p>
+                No locations exist. Please add locations and return here to set CCC codes.
+            </p>
+        </c:otherwise>
+    </c:choose>
 </div>
-<%@ include file="/WEB-INF/template/footer.jsp"%>
+
+<%@ include file="/WEB-INF/template/footer.jsp" %>
